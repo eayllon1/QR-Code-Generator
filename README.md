bf8e6883da7f9719c0ec360850ff840c5831fce4
eayllon1 <eayllon1@gmail.com>
2019-04-16 20:03:36 -0400

    Added QR code generator

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # QR Code Generator
+
+## 1 | Description
+
+Generates a QR code for both React (web) and React Native.
+
+## 2 | Getting Started
+
+### 2.1 Installing
+
+Clone, then run
+
+```
+npm install
+```
+
+inside the directory.
+
+You can alter the value of the QR code inside `App.js`.
+
+### 2.2 Running
+
+To run in web:
+
+```
+npm run web
+```
+
+To run in mobile with Expo and mobile simulator:
+
+```
+npm run start
+```
--- src/components/QRcode.web.js
+++ src/components/QRcode.web.js
@@ -2,13 +2,13 @@ import React, { Component } from 'react'
 import {
   View
 } from 'react-native'
-import QRCode from 'react-native-qrcode-svg';
+import QRCode from 'qrcode.react';
 
 export default class Code extends Component {
   render() {
     return (
       <View>
-        <QRCode size={this.props.size} value={this.props.value}/>
+        <QRCode style = {{width: this.props.size, height: this.props.size}} value={this.props.value}/>
       </View>
     )
   }
